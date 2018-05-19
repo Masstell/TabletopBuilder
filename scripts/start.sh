a723959fef756f6cc9d102605a069e2512624bac
Travis Chase <travis.chase@gmail.com>
2018-05-19 17:22:58 -0600

    added node in start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./node_modules/webpack-serve/cli.js --config config/webpack.config.development.js --open
+node ./node_modules/webpack-serve/cli.js --config config/webpack.config.development.js --open
