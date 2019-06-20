611a913f0d2cb1e157b32ae98baa71ce292e7c0e
gaudeon <gaudeon@cpan.org>
2019-06-20 16:00:19 -0600

    added babel node

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-node ./node_modules/nodemon/bin/nodemon.js --watch server --exec ./node_modules/@babel/cli/bin/babel.js ./server/index.js --ignore src
+node ./node_modules/nodemon/bin/nodemon.js --watch server --exec ./node_modules/@babel/node/bin/babel-node.js ./server/index.js --ignore src
