f8816cea093f02e86bddcb03364ba54ef3a2c551
DizzyDogg <jeffthetenor@yahoo.com>
2019-06-20 14:37:49 -0600

    let new cards get generated from playingcard

--- src/scenes/play-game.js
+++ src/scenes/play-game.js
@@ -5,35 +5,37 @@ import Wallet from "../blackjack/wallet";
 import Deck from '../blackjack/deck';
 
 export default class PlayGameScene extends Phaser.Scene {
-  constructor(config, key = "PlayGame") {
-    super({ key: key });
-  }
+    constructor(config, key = "PlayGame") {
+        super({ key: key });
+    }
 
-  init() {}
+    init() {}
 
-  preload() {
-    // load all the resources required for this scene before using them
-    this.load.image("pokerchip1", "pokerchip1.png");
-    this.load.image("table", "background.png");
-  }
+    preload () {
+        // load all the resources required for this scene before using them
+        this.load.image('pokerchip1', 'pokerchip1.png');
+        this.load.image("table", "background.png");
+        this.load.image('cards', 'face_cards.png');
+        this.load.image('card_backs', 'face_cards_back.png');
+    }
 
-  create() {
-    let deck = new Deck(this);
-    console.log(deck.getCollection());
-    this.add.image(300, 300, "table");
-    let chip = new Chip({
-      imageProps: {
-        scene: this,
-        x: 50,
-        y: 50
-      }
-    });
-    this.add.existing(chip.image);
-    let bank = new Bank();
-    let wallet = new Wallet();
+    create() {
+        let deck = new Deck(this);
+        console.log(deck.getCollection());
+        this.add.image(300, 300, "table");
+        let chip = new Chip({
+            imageProps: {
+                scene: this,
+            x: 50,
+            y: 50
+            }
+        });
+        this.add.existing(chip.image);
+        let bank = new Bank();
+        let wallet = new Wallet();
 
-    console.log(bank, wallet);
-  }
+        console.log(bank, wallet);
+    }
 
-  update() {}
+    update() {}
 }
