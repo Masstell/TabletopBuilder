3909247b3e940444f2906a644978737676bcac0d
Joe Broderick <PenguinMan98@usa.net>
2019-06-20 15:49:19 -0600

    Removed deck

--- src/blackjack/player.js
+++ src/blackjack/player.js
@@ -1,12 +1,10 @@
 import Player from "../abstracts/player.js";
-import Deck from "./deck";
 import Hand from "./hand";
 import Wallet from "./wallet";
 // player imports Deck
 // player imports Hand
 // player imports wallet, but I don't need that here
 
-Player.deck = {};
 Player.hand = {};
 Player.wallet = {};
 
