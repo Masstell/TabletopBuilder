fd5283a1cbe454d1f6de734620179d6cdf056a6c
DizzyDogg <jeffthetenor@yahoo.com>
2019-06-20 11:26:27 -0600

    update card

--- src/abstracts/card.js
+++ src/abstracts/card.js
@@ -1,16 +1,16 @@
+class Card {
 
-let card = {};
+    constructor(img, name, desc, value) {
+        // has a sprite
+        this.img = img;
 
-function new(img, name, desc, value) {
-    // has a sprite
-    card.img = img;
+        // has a name
+        this.name = desc || 'newCard';
 
-    // has a name
-    card.name = desc || 'newCard';
+        // has a description
+        this.desc = desc || 'Description of my Card';
 
-    // has a description
-    card.desc = desc || 'Description of my Card';
-
-    // has a value
-    card.value = value || 1;
+        // has a value
+        this.value = value || 1;
+    }
 }
